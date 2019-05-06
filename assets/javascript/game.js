4978c86abca7dc2ab67c41d16288dc2276b8e84e
Morris, Michael <mmorris1990@gmail.com>
2019-05-06 18:11:00 -0500

    chose theme and created variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+// Words Array
+var words = ["stanleycup" , "hattrick" , "slapshot" , "faceoff" , "powerplay" , "penaltybox" , "icing" , "overtime"]
+
+// Game Status Variable
+var guessRemaining = 8;
+var wins = 0;
+var losses = 0;
+
+// Variables used to run the script
+var randomWord = "";
+var wordLength = [];
+var blanks = 0;
+var blanksAndGuesses = [];
+var badGuess = [];
